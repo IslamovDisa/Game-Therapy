5cdb44d3c904104b8e42fe897c8797d59dae6110
Denis Islamov <islamov.denis@gmail.com>
2022-05-26 00:45:27 +0800

    add base animation

---
+++ Assets/GameTherapy/Scripts/AnimatedActor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimatedActor : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Animator _animator;
+
+    public float Speed;
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", _agent.desiredVelocity.magnitude / _agent.acceleration);
+    }
+}
